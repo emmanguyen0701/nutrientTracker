0285b01aa7247b9f44d32888c0814e639ee7a8f6
Emma Nguyen <emma.nguyen.0701@gmail.com>
2022-06-25 07:26:59 -0400

    Added MongoDB connection string

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ import config from '../config/config'
 import app from './express'
 
 
-const db = mongoose.connect(config.mongoUri)
+const db = mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
         .then(() => {'Database connected'})
         .catch((err) => console.log(err))
 
